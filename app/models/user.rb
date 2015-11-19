f57c69876f396b027402e427b477fcf47a102359
Tceh <tima.jet@yandex.ru>
2015-11-19 21:37:58 +0300

    Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
-	devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :confirmable, :validatable
+
 
   has_one :profile
   has_many :posts
